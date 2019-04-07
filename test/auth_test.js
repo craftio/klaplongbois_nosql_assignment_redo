05ccebaee54d7b9fbef79ab5f155d19495c06db0
Sam Glerum <samuelglerum@hotmail.com>
2019-04-07 20:23:59 +0200

    written tests for users

--- test/auth_test.js
+++ test/auth_test.js
@@ -12,13 +12,24 @@ const User = require('../src/user');
 const user = require('../data/userRepo');
 
 // TESTS
-xdescribe('Creating records', () => {
-    it('saves a user in the database', (done) => {
+describe('Creating records', () => {
+    it('should return a 201 when saving a user in the database', (done) => {
         chai.request(server)
             .post('/api/users')
-            .send({ username: 'testUser', password: 'test1234'})
+            .send({ "username": "testUser", "password": "test1234"})
             .end((err, res) => {
-                res.body.statuscode.should.be.equal(201);
+                res.should.have.status(201);
+                done();
+            })
+    });
+
+    it('should return a 409 when the supplied username is already present in the database', (done) => {
+        chai.request(server)
+            .post('/api/users')
+            .send({ "username": "testUser", "password": 'test1234'})
+            .end((err, res) => {
+                res.should.have.status(409);
+                done();
             })
     });
 
@@ -37,4 +48,81 @@ xdescribe('Creating records', () => {
                     });
             });
     });
+});
+
+describe('Changing user records', () => {
+
+    it('should return a 400 when missing username on changing user password', (done) => {
+        chai.request(server)
+            .put('/api/users')
+            .send({ "username": null, "password": "test1234", "newPassword": "test1234"})
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+    });
+
+    it('should return a 400 when missing password on changing user password', (done) => {
+        chai.request(server)
+            .put('/api/users')
+            .send({ "username": "testUser", "password": null, "newPassword": "test1234"})
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+    });
+
+    it('should return a 400 when missing new password on changing user password', (done) => {
+        chai.request(server)
+            .put('/api/users')
+            .send({ "username": "testUser", "password": "testpassword", "newPassword": null})
+            .end((err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+    });
+
+    it('should return a 422 when the supplied username does not exist', (done) => {
+        chai.request(server)
+            .put('/api/users')
+            .send({ "username": "usernamedoesnotexist", "password": "test1234", "newPassword": "test1234"})
+            .end((err, res) => {
+                res.should.have.status(422);
+                done();
+            })
+    });
+
+    it('should return a 401 when a user is not logged in', (done) => {
+        chai.request(server)
+            .put('/api/users')
+            .send({ "username": "testUser", "password": "test12qwe34", "newPassword": "test12342"})
+            .end((err, res) => {
+                res.should.have.status(401);
+                done();
+            })
+
+    })
+});
+
+describe('Deleting user records', () => {
+
+    it('Should return a 422 when the user does not exist', (done) => {
+        chai.request(server)
+            .delete('/api/users')
+            .send({"username": "userthatdoesnotexist", "password": "testpassword"})
+            .end((err, res) => {
+                res.should.have.status(422);
+                done();
+            })
+    });
+
+    it('Should return a 401 when the supplied password is not correct', (done) => {
+        chai.request(server)
+            .delete('/api/users')
+            .send({"username": "testUser", "password": "incorrectpassword"})
+            .end((err, res) => {
+                res.should.have.status(401);
+                done();
+            })
+    })
 });
