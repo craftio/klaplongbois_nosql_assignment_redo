7fcac4fb97167b0c0771baeb849cc78cdf374f51
Sam Glerum <samuelglerum@hotmail.com>
2019-04-07 21:32:07 +0200

    written tests for friendship

--- test/friendship_test.js
+++ test/friendship_test.js
@@ -6,3 +6,55 @@ const server = require('../test_index');
 
 chai.use(chaiHttp);
 chai.should();
+
+describe('Creating friendships', () => {
+    it('Should return a 200 on creating a friendship', (done) => {
+        chai.request(server)
+            .post('/api/friendships')
+            .send({"name": "test1", "friendname": "test2"})
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+    });
+
+    it('Should return a 422 when username does not exist', (done) => {
+        chai.request(server)
+            .post('/api/friendships')
+            .send({"name": "doesnotexist", "friendname": "test2"})
+            .end((err, res) => {
+                res.should.have.status(422);
+                done();
+            })
+    });
+
+    it('Should return a 422 when friendname does not exist', (done) => {
+        chai.request(server)
+            .post('/api/friendships')
+            .send({"name": "test1", "friendname": "doesnotexist"})
+            .end((err, res) => {
+                res.should.have.status(422);
+                done();
+            })
+    })
+});
+
+describe('Reading friendships', () => {
+    it('Should return a 200 on a GET request to /api/friendships', (done) => {
+        chai.request(server)
+            .get('/api/friendships')
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+    });
+
+    it('Should return a 200 on a GET request to /api/friendships/1', (done) => {
+        chai.request(server)
+            .get('/api/friendships')
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+    })
+});
