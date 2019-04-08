b832ff4e357b1d0caadb3b3b5961b634178bc42c
btaks <btaks@avans.nl>
2019-04-08 05:29:44 +0200

    Fixing commenting.

--- data/commentRepo.js
+++ data/commentRepo.js
@@ -26,29 +26,20 @@ module.exports = class StudditComments {
             .then((user) => {
                 Thread.findOne({ _id: id })
                     .then((thread) => {
-                        if (thread !== null && thread !== undefined) {
-                            const newComment = new Comment({
-                                postedBy: user,
-                                onThread: thread,
-                                content: content
+                        thread.comments.push({
+                            user: user._id,
+                            content: content
+                        });
+                        thread.save()
+                            .then(() => {
+                                res.status(200).json(new jsonModel("/api/comments/threadId", "POST", 200, "Succesfully commented on thread."));
                             });
-                            thread.comments.push(newComment);
-                            Promise.all([user.save(), thread.save(), newComment.save()])
-                                .then(() => {
-                                    res.status(201).json(new jsonModel("/api/comments", "POST", 201, "Comment has been succesfully created"));
-                                })
-                                .catch(() => {
-                                    res.status(500).json(ApiErrors.internalServerError());
-                                })
-                        } else {
-                            res.status(409).json(new jsonModel("/api/user", "POST", 409, "You can't comment on an non-existing thread."));
-                        }
                     })
                     .catch(() => {
                         res.status(404).json(ApiErrors.notFound());
                     })
                 .catch(() => {
-                    res.status(422).json("/api/user", "POST", 422, "You can't comment on an non-existing thread.");
+                    res.status(422).json("/api/comments/:threadId", "POST", 422, "You can't comment on an non-existing thread.");
                 })
             })
     }
